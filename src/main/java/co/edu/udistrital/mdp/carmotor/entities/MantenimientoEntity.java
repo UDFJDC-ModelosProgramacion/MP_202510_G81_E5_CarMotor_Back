4b8f8b79b721a544168b1b0fe14288e988ce0d60
Brayancas017 <brayangogar@gmail.comgit config --global user.email brayangogar@gmail.com>
2025-04-29 14:19:59 -0500

    asociacion 1a1 taller y mantenimiento

--- src/main/java/co/edu/udistrital/mdp/carmotor/entities/MantenimientoEntity.java
+++ src/main/java/co/edu/udistrital/mdp/carmotor/entities/MantenimientoEntity.java
@@ -1,12 +1,19 @@
 package co.edu.udistrital.mdp.carmotor.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
 import lombok.Data;
+import uk.co.jemos.podam.common.PodamExclude;
 
 @Data
 @Entity
 public class MantenimientoEntity extends BaseEntity{
     
+    //1 a 1 con tallerentity
+    @PodamExclude
+    @OneToOne(mappedBy = "mantenimiento")
+    private TallerEntity taller;
+
     private String fechaDeServicio;
     private String tipoDeServicio;
 }
--- src/main/java/co/edu/udistrital/mdp/carmotor/entities/TallerEntity.java
+++ src/main/java/co/edu/udistrital/mdp/carmotor/entities/TallerEntity.java
@@ -1,11 +1,19 @@
 package co.edu.udistrital.mdp.carmotor.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
 import lombok.Data;
+import uk.co.jemos.podam.common.PodamExclude;
 
 @Data
 @Entity
 public class TallerEntity extends BaseEntity {
+
+    //1 a 1 con mantenimiento
+    @PodamExclude
+    @OneToOne
+    private MantenimientoEntity mantenimiento;
+
     private String direccion;
     private String numero;
     private String horario;
